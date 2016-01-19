85abe7e56dd22a0afe39476d1debf84aa8734b14
Joseph Vano <joseph@vano.io>
2016-01-18 23:01:27 -0500

    add ArticleComponent and render a single item on page

--- app.ts
+++ app.ts
@@ -1,8 +1,67 @@
 import { bootstrap }  from "angular2/platform/browser";
 import { Component }  from "angular2/core";
 
+
+@Component({
+  selector: 'reddit-article',
+  host: {
+    class: 'row',
+  },
+  template: `
+    <div class="four wide column center aligned votes">
+      <div class="ui statistic">
+        <div class="value"> {{ votes }} </div>
+        <div class="label"> Points </div>
+      </div>
+    </div>
+
+    <div class="twelve wide column">
+
+      <a class="ui large header" href="{{ link }}"> {{ title }} </a>
+      <ul class="ui big horizontal list voters">
+        <li class="item">
+          <a href (click)="voteUp()">
+          <i class="arrow up icon"></i> upvote
+          </a>
+        </li>
+        <li class="item">
+          <a href (click)="voteDown()">
+            <i class="arrow down icon"></i> downvote
+          </a>
+        </li>
+      </ul>
+    </div>
+  `
+
+})
+class ArticleComponent{
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor(){
+    this.title = "Angular 2";
+    this.link = "http://angular.io";
+    this.votes = 10;
+  }
+
+  voteUp(): boolean{
+    this.votes+=1;
+
+    return false;
+  }
+
+  voteDown(): boolean{
+    this.votes-=1;
+
+    return false;
+  }
+}
+
+
 @Component({
   selector: 'reddit',
+  directives: [ArticleComponent],
   template: `
   <form class="ui large form segment">
     <h3 class="ui header">Add a Link</h3>
@@ -17,6 +76,10 @@ import { Component }  from "angular2/core";
 
     <button (click)="addArticle(newtitle, newlink)" class="ui positive right floated button">Submit link</button>
   </form>
+
+  <div class="ui grid posts">
+    <reddit-article></reddit-article>
+  </div>
   `
 })
 class RedditApp{
