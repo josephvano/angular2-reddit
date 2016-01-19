9a184591ac1226ca76737f9a2f7df879cbb0e935
Joseph Vano <joseph@vano.io>
2016-01-18 22:38:25 -0500

    changed hello-world to reddit-app component and added form html/stylings from index_full.html

--- app.ts
+++ app.ts
@@ -1,21 +1,32 @@
 import { bootstrap }  from "angular2/platform/browser";
 import { Component }  from "angular2/core";
-import { NgFor }      from "angular2/common";
 
 @Component({
-  selector: 'hello-world',
+  selector: 'reddit',
   template: `
-  <ul>
-    <li *ngFor="#name of names">Hello {{ name }}</li>
-  </ul>
+  <form class="ui large form segment">
+    <h3 class="ui header">Add a Link</h3>
+    <div class="field">
+      <label for="title">Title: </label>
+      <input name="title" #newtitle>
+    </div>
+    <div class="field">
+      <label for="link">Link: </label>
+      <input name="link" #newlink>
+    </div>
+
+    <button (click)="addArticle(newtitle, newlink)" class="ui positive right floated button">Submit link</button>
+  </form>
   `
 })
-class HelloWorld{
-  names: string[];
+class RedditApp{
 
   constructor(){
-    this.names = ["Emma", "Arya", "Joey"];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): void{
+    console.log(`Adding article ${title.value} and link: ${link.value}`);
   }
 }
 
-bootstrap(HelloWorld);
+bootstrap(RedditApp);
