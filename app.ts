7b9afcabe83270ca998f0267ac24ecf3a1493c87
Joseph Vano <joseph@vano.io>
2016-01-18 23:20:24 -0500

    refactor ArticleComponent to accept 'article' as an input and loop over articles to create reddit-article components

--- app.ts
+++ app.ts
@@ -24,6 +24,7 @@ class Article{
 
 @Component({
   selector: 'reddit-article',
+  inputs: ['article'],
   host: {
     class: 'row',
   },
@@ -55,10 +56,7 @@ class Article{
 
 })
 class ArticleComponent{
-
-  constructor(){
-    this.article = new Article('Angular 2', 'http://angular.io', 10);
-  }
+  article: Article;
 
   voteUp(): boolean{
     this.article.voteUp();
@@ -93,13 +91,19 @@ class ArticleComponent{
   </form>
 
   <div class="ui grid posts">
-    <reddit-article></reddit-article>
+    <reddit-article *ngFor="#article of articles" [article]="article"></reddit-article>
   </div>
   `
 })
 class RedditApp{
+  articles: Article[];
 
   constructor(){
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullsack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1)
+    ];
   }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): void{
